ec5038b7a02a6f5d377eafd3f1e066cd780c20a6
Tobet <t.betti.developer@gmail.com>
2019-10-08 00:26:46 +0200

    standard template php tutorial

--- index.php
+++ index.php
@@ -1,28 +1,4 @@
 <?php
-$radius = 25;
-$pi = 3.14;
-$age = 20;
-
-//echo $pi * $radius**2
-
-// order of operation (B I D M A S)
-
-//echo 2 * +(4 + 9) / 3;
-
-// increment and decrement operators
-
-//echo $radius++;
-//echo $radius;
-
-//$age += 10;
-//$age -= 10;
-//$age *= 2;
-//echo $age
-
-// number
-//echo floor($pi);
-//echo ceil($pi);
-echo pi();
 
 ?>
 
