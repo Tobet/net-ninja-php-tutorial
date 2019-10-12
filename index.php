ed72ca4b3a598f6d761a5e89e6f0c4393335af4c
Tobet <t.betti.developer@gmail.com>
2019-10-12 18:55:55 +0200

    loops module

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
 
+// comparisons booleans (true or false)
+//echo true;
+//echo false;
+
+// numbers
+//echo 5 < 10;
+//echo 5 > 10;
+//echo 5 == 10;
+//echo 10 == 10;
+//echo 5 != 10;
+//echo 5 <= 5;
+//echo 5 >= 5;
+
+// strings
+//echo 'shaun' < 'yoshi'; // true
+//echo 'shaun' > 'yoshi'; // false
+//echo 'shaun' > 'Shaun'; // true
+//echo 'mario' == 'mario'; // true
+//echo 'mario' == 'Mario'; // false
+
+// loose vs strict equal comparison
+//echo 5 == '5'; // true
+//echo 5 === '5'; // false
+//echo true == '1'; // true
+//echo false == ''; // true
+
 ?>
 
 <!DOCTYPE html>
