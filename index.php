c96c4c9cc62f2f4e58574150db0e51e481052bf8
Tobet <t.betti.developer@gmail.com>
2019-11-26 00:03:50 +0100

    connected app to database

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
 
+// connect to database
+$conn = mysqli_connect('localhost', 'tobet', 'tobet', 'ninja_pizza');
+
+// check connection
+if (!$conn) {
+    echo 'Connection error: ' . mysqli_connect_error();
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include ('templates/header.php'); ?>
+<?php include('templates/header.php'); ?>
 
-<?php include ('templates/footer.php'); ?>
+<?php include('templates/footer.php'); ?>
 
 </html>
